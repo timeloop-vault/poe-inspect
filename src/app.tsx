7db692624643752c28646f3557cad9b4f6816f86
Stefan Hagdahl <stefan@timeloop.se>
2022-02-22 23:34:26 +0100

    Initial build with Tauri and Vite/Preact-ts

---
+++ src/app.tsx
@@ -0,0 +1,19 @@
+import { h } from "preact";
+
+export function App(): h.JSX.Element {
+  return (
+    <>
+      <p>Hello Vite + Preact! </p>
+      <p>
+        <a
+          class="link"
+          href="https://preactjs.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Preact
+        </a>
+      </p>
+    </>
+  );
+}
