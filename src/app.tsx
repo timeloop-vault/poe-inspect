e27cce781916dc8e5f19fe8f32e28c0547e09736
Stefan Hagdahl <stefan@timeloop.se>
2022-02-23 23:53:02 +0100

    Advance copy paste from PoE
    Use enigo in rust to send ctrl+alt+c to PoE
    Read clipboard in webapp when command has been executed
    Print advance copy paste in console

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,67 @@
-import { h } from "preact";
+/* eslint-disable no-redeclare */
+import { isRegistered, register } from "@tauri-apps/api/globalShortcut";
+import { Component, h } from "preact";
+import { invoke } from "@tauri-apps/api/tauri";
+import { clipboard } from "@tauri-apps/api";
 
-export function App(): h.JSX.Element {
-  return (
-    <>
-      <p>Hello Vite + Preact! </p>
-      <p>
-        <a
-          class="link"
-          href="https://preactjs.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Preact
-        </a>
-      </p>
-    </>
-  );
+type Props = {};
+type State = {
+  count: number;
+};
+
+class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+    this.handleCtrlD = this.handleCtrlD.bind(this);
+  }
+
+  handleCtrlD(): void {
+    console.log("Ctrl+D pressed");
+    clipboard.writeText("");
+    invoke("send_adv_copy").then(() => {
+      clipboard.readText().then((text) => {
+        console.log("text copied", text);
+      });
+    });
+    const { count } = this.state;
+    this.setState({
+      count: count + 1,
+    });
+  }
+
+  componentDidMount(): void {
+    isRegistered("Ctrl+D")
+      .then((registered) => {
+        if (!registered) {
+          register("Ctrl+D", this.handleCtrlD);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  render(): h.JSX.Element {
+    const { count } = this.state;
+    return (
+      <>
+        <p>Hello Vite + Preact! {count}</p>
+        <p>
+          <a
+            class="link"
+            href="https://preactjs.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Preact
+          </a>
+        </p>
+      </>
+    );
+  }
 }
+
+export default App;
