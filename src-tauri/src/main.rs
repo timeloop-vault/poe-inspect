de8c51a2c198b2951e52547b0154643ef76eefc2
Stefan Hagdahl <stefan.hagdahl83@gmail.com>
2022-02-23 23:53:02 +0100

    Advance copy paste from PoE
    Use enigo in rust to send ctrl+alt+c to PoE
    Read clipboard in webapp when command has been executed
    Print advance copy paste in console

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,24 @@
   windows_subsystem = "windows"
 )]
 
+use enigo::*;
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![send_adv_copy])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+#[tauri::command(async)]
+fn send_adv_copy() {
+  let c: char = 'c';
+  let mut enigo = Enigo::new();
+  enigo.key_down(Key::Control);
+  enigo.key_down(Key::Alt);
+  enigo.key_down(Key::Layout(c));
+  enigo.key_up(Key::Layout(c));
+  enigo.key_up(Key::Alt);
+  enigo.key_up(Key::Control);
+  println!("I was invoked from JS!");
 }
